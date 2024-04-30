495ab9db693436a2f86e94734989a1cb505600e1
wonjun0120 <wonjundero@gmail.com>
2024-04-30 14:27:18 +0900

    feat: 프로젝트 초기 세팅
    start.spring.io 에서 스프링부트 초기 세팅 진행
    Gradle, Springboot v3.1.11, Java JDK 17, JAR
    Spring Web, H2 Database, Spring Data JPA, Mustache,  Lombok
    gitignore.io에서 gitignore 추가, Java Gradle에 맞춰 생성

---
+++ src/main/java/com/wonjun/blogboard/BlogboardApplication.java
@@ -0,0 +1,13 @@
+package com.wonjun.blogboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogboardApplication.class, args);
+	}
+
+}
