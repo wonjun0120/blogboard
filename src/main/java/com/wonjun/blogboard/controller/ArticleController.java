dd7a8b701d40cd221113c5ddad27f644f5423dd5
wonjun0120 <wonjundero@gmail.com>
2024-04-30 16:16:06 +0900

    refactor: lombok 적용, 로깅 적용
    컨트롤러 로깅 적용, 엔티티, DTO 롬복 적용

--- src/main/java/com/wonjun/blogboard/controller/ArticleController.java
+++ src/main/java/com/wonjun/blogboard/controller/ArticleController.java
@@ -3,11 +3,13 @@ package com.wonjun.blogboard.controller;
 import com.wonjun.blogboard.dto.ArticleForm;
 import com.wonjun.blogboard.entity.Article;
 import com.wonjun.blogboard.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j
 @Controller
 public class ArticleController {
     @Autowired // 스프링부트가 미리 생성해 놓은 리파지터리 객체 주입, DI
@@ -20,13 +22,15 @@ public class ArticleController {
 
     @PostMapping("/articles/create")
     public String craeteArticle(ArticleForm form){
-        System.out.println(form.toString());
+        log.info(form.toString());
+        
         // DTO 엔티티로 변환
         Article article = form.toEntity();
-        System.out.println(article.toString());
+        log.info(article.toString());
+
         // 리파지터리로 엔티티 DB 저장
         Article saved = articleRepository.save(article);
-        System.out.println(saved.toString());
+        log.info(saved.toString());
         return "";
     }
 }
