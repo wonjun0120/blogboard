cb331024792bafe53ec236c1a36f0e4238344845
wonjun0120 <wonjundero@gmail.com>
2024-04-30 17:30:24 +0900

    feat: 게시글 수정, 삭제 기능 추가
    게시글 수정, 삭제 기능 추가 및 관련 페이지 수정

--- src/main/java/com/wonjun/blogboard/controller/ArticleController.java
+++ src/main/java/com/wonjun/blogboard/controller/ArticleController.java
@@ -6,8 +6,13 @@ import com.wonjun.blogboard.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -31,6 +36,57 @@ public class ArticleController {
         // 리파지터리로 엔티티 DB 저장
         Article saved = articleRepository.save(article);
         log.info(saved.toString());
-        return "";
+        return "redirect:/articles/" + saved.getId();
+    }
+
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        log.info("id = " + id);
+
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        model.addAttribute("article", articleEntity);
+        return "articles/show";
+    }
+
+    @GetMapping("/articles")
+    public String index(Model model){
+        List<Article> articleEntityList = articleRepository.findAll();
+        model.addAttribute("articleList", articleEntityList);
+        return "articles/index";
+    }
+
+    @GetMapping("/articles/{id}/edit")
+    public String edit(@PathVariable Long id, Model model){
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        model.addAttribute("article", articleEntity);
+        return "articles/edit";
+    }
+
+    @PostMapping("/articles/update")
+    public String update(ArticleForm form){
+        log.info(form.toString());
+
+        Article articleEntity = form.toEntity();
+        log.info(articleEntity.toString());
+
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
+        if (target != null){
+            articleRepository.save(articleEntity);
+        }
+
+        return "redirect:/articles/" + articleEntity.getId();
+    }
+
+    @GetMapping("/articles/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr) {
+        log.info("삭제 요청");
+
+        Article target = articleRepository.findById(id).orElse(null);
+        log.info(target.toString());
+        if(target != null) {
+            articleRepository.delete(target);
+            rttr.addFlashAttribute("msg", "삭제되었습니다.");
+        }
+        return "redirect:/articles";
     }
 }
--- src/main/java/com/wonjun/blogboard/repository/ArticleRepository.java
+++ src/main/java/com/wonjun/blogboard/repository/ArticleRepository.java
@@ -3,5 +3,9 @@ package com.wonjun.blogboard.repository;
 import com.wonjun.blogboard.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface ArticleRepository extends CrudRepository<Article, Long> {
+    @Override
+    ArrayList<Article> findAll();
 }
