3b0026b1e4c52a09ea3bb4b3862bc389ca113bbc
wonjun0120 <wonjundero@gmail.com>
2024-05-01 22:33:08 +0900

    feat: Comment Entity Repository 생성
    Comment Repository 생성
    find by article id, find by nickname 생성
    Comment Entity 생성

---
+++ src/main/java/com/wonjun/blogboard/repository/CommentRepository.java
@@ -0,0 +1,24 @@
+package com.wonjun.blogboard.repository;
+
+import com.wonjun.blogboard.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    @Query(value =
+            "SELECT * " +
+            "FROM comment " +
+            "WHERE article_id = :articleId",
+            nativeQuery = true)
+    List<Comment> findByArticleId(Long articleId);
+
+    @Query(value =
+            "SELECT * " +
+            "FROM comment " +
+            "WHERE nickname = :nickname",
+            nativeQuery = true)
+    List<Comment> findByNickname(String nickname);
+
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,7 @@
 INSERT INTO article(title, content) VALUES ('가가가가', '1111');
 INSERT INTO article(title, content) VALUES ('나나나나', '2222');
 INSERT INTO article(title, content) VALUES ('다다다다', '3333');
+
+INSERT INTO comment(article_id, nickname, body) VALUES (3, '나다', '다?');
+INSERT INTO comment(article_id, nickname, body) VALUES (3, '나다다', '다다?');
+INSERT INTO comment(article_id, nickname, body) VALUES (3, '나다다다', '다다다다?');
