d32001debb391a985c0170e443142ac54ae8d54f
wonjun0120 <wonjundero@gmail.com>
2024-05-05 13:39:34 +0900

    feat: comment post 기능 생성

--- src/main/java/com/wonjun/blogboard/controller/CommentApiController.java
+++ src/main/java/com/wonjun/blogboard/controller/CommentApiController.java
@@ -5,9 +5,7 @@ import com.wonjun.blogboard.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +19,12 @@ public class CommentApiController {
         List<CommentDto> dtos = commentService.comments(articleId);
 
         return ResponseEntity.status(HttpStatus.OK).body(dtos);
+    }
+
+    @PostMapping("/api/articles/{articleId}/comments")
+    public ResponseEntity<CommentDto> create (@PathVariable Long articleId,
+                                              @RequestBody CommentDto dto) {
+        CommentDto commentDto = commentService.create(articleId, dto);
+        return ResponseEntity.status(HttpStatus.OK).body(commentDto);
     }
 }
--- src/main/java/com/wonjun/blogboard/service/CommentService.java
+++ src/main/java/com/wonjun/blogboard/service/CommentService.java
@@ -1,12 +1,15 @@
 package com.wonjun.blogboard.service;
 
 import com.wonjun.blogboard.dto.CommentDto;
+import com.wonjun.blogboard.entity.Article;
 import com.wonjun.blogboard.entity.Comment;
 import com.wonjun.blogboard.repository.ArticleRepository;
 import com.wonjun.blogboard.repository.CommentRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.beans.Transient;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,5 +27,16 @@ public class CommentService {
                 .stream()
                 .map(comment -> CommentDto.createCommentDto(comment))
                 .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public CommentDto create(Long articleId, CommentDto dto) {
+        Article article = articleRepository.findById(articleId)
+                .orElseThrow(() -> new IllegalArgumentException("댓글 생성 실패하였습니다." +
+                        "대상 게시글이 없습니다."));
+
+        Comment comment = Comment.createComment(dto, article);
+        Comment created = commentRepository.save(comment);
+        return CommentDto.createCommentDto(created);
     }
 }
