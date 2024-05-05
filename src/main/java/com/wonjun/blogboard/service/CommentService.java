5e941c65383dad5a91a9111e99be37067e1f218e
wonjun0120 <wonjundero@gmail.com>
2024-05-05 13:22:09 +0900

    refactor: comment 서비스 로직 변경, for -> stream 변경

--- src/main/java/com/wonjun/blogboard/service/CommentService.java
+++ src/main/java/com/wonjun/blogboard/service/CommentService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CommentService {
@@ -19,14 +20,9 @@ public class CommentService {
     private ArticleRepository articleRepository;
 
     public List<CommentDto> comments(Long articleId) {
-        List<Comment> comments = commentRepository.findByArticleId(articleId);
-        List<CommentDto> dtos = new ArrayList<CommentDto>();
-        for(int i = 0; i < comments.size(); i++) {
-            Comment c = comments.get(i);
-            CommentDto dto = CommentDto.createCommentDto(c);
-            dtos.add(dto);
-        }
-
-        return dtos;
+        return commentRepository.findByArticleId(articleId)
+                .stream()
+                .map(comment -> CommentDto.createCommentDto(comment))
+                .collect(Collectors.toList());
     }
 }
